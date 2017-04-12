82c5484f59a8b0724ca8e7030c8cea58cf93b3e1
Joseph <joseph.isbir@gmail.com>
2017-04-12 00:41:37 -0300

    Implementação do cadastro de usuário.

---
+++ inscrevase.php
@@ -0,0 +1,135 @@
+<?php 
+
+$erro_cpf		= isset($_GET['erro_cpf'])		? $_GET['erro_cpf'] 	: 0;
+$erro_email 	= isset($_GET['erro_email'])	? $_GET['erro_email']	: 0;
+$erro_senha 	= isset($_GET['erro_senha']) 	? $_GET['erro_senha']	: 0;
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>Twitter clone</title>
+		
+		<!-- jquery - link cdn -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+		<!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <img src="imagens/icone_twitter.png" />
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="index.php">Voltar para Home</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+
+
+	    <div class="container">
+	    	
+	    	<br /><br />
+
+	    	<div class="col-md-4"></div>
+	    	<div class="col-md-4">
+	    		<h3>Cadastre-se.</h3>
+	    		<br />
+				<form method="post" action="registra_usuario.php" id="formCadastrarse">
+					<div class="form-group">
+						<input type="text" class="form-control" id="cpf" name="cpf" placeholder="CPF" 
+								maxlength="14" required="requiored" onkeypress="formatar('###.###.###-##', this);" >
+						
+						<?php 
+						
+							if ($erro_cpf){ // 1 é true e 0 é false
+								echo '<font style="color:#FF0000"> CPF já existe</font>';
+							}
+						
+						?>
+						
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" required="requiored">
+					</div>
+
+					<div class="form-group">
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email" required="requiored">
+						
+						<?php 
+						
+						if ($erro_email){ // 1 é true e 0 é false
+							echo '<font style="color:#FF0000"> Email já existe</font>';
+							}
+						
+						?>
+						
+					</div>
+					<div class="form-group">
+						<input type="date" class="form-control" id="dtNascimento" name="dtNascimento" placeholder="Data de Nascimento" required="requiored">
+					</div>
+					
+					<div class="form-group">
+						<input type="password" class="form-control" id="senha" name="senha" placeholder="Senha" required="requiored">
+					</div>
+					
+					<div class="form-group">
+						<input type="password" class="form-control" id="confirmSenha" name="confirmSenha" placeholder="Confirme a Senha" required="requiored">
+					
+						<?php 
+						
+						if ($erro_senha){ // 1 é true e 0 é false
+							echo '<font style="color:#FF0000"> Confirmação de senha não confere</font>';
+							}
+						
+						?>
+					
+					</div>
+					
+					<button type="submit" class="btn btn-primary form-control">Cadastrar</button>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+
+			<div class="clearfix"></div>
+			<br />
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+
+		</div>
+
+
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script>  function formatar(mascara, documento){   
+					var i = documento.value.length;   
+					var saida = mascara.substring(0,1);   
+					var texto = mascara.substring(i)     
+					if (texto.substring(0,1) != saida){    
+						documento.value += texto.substring(0,1);   
+					}    
+				}
+		</script>
+	
+	</body>
+</html>
---
+++ registra_usuario.php
@@ -0,0 +1,99 @@
+<?php
+
+	require_once('db.class.php');
+
+	$cpf = $_POST['cpf'];
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$dtNascimento = $_POST['dtNascimento'];
+	$senha = md5($_POST['senha']);	
+	$confirmSenha= md5($_POST['confirmSenha']);	
+
+	$verificar = array(".", "-");
+	$substituir   = array("", "");
+	$cpfSemMascara = str_replace($verificar, $substituir, $cpf);
+
+	$objDB = new db();
+	$link = $objDB->conecta_mysql();
+	
+	$cpf_existe = false;
+	$email_existe = false;
+	$senha_confirmada = false;
+	
+	//verifica confirmação de senha
+	if ($senha != $confirmSenha){
+		$senha_confirmada = true;
+	}
+	
+	//verificar se o CPF já existe
+	$sql = " SELECT * FROM maf.tb_pessoa WHERE cpf_pessoa = '$cpfSemMascara' ";
+	if($resultado_id = mysqli_query($link, $sql)){
+		
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		
+		if (isset($dados_usuario['CPF_PESSOA'])){
+			//echo 'Usuário já cadastrado';
+			$cpf_existe= true;	
+		}
+		
+	} else {
+		echo 'Erro ao tentar localizar o registro de CPF';
+	}
+	
+	//verificar se o email já existe
+	$sql = " SELECT * FROM maf.tb_pessoa WHERE email_pessoa = '$email' ";
+	if($resultado_id = mysqli_query($link, $sql)){
+		
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		
+		if (isset($dados_usuario['EMAIL_PESSOA'])){
+			//echo 'E-mail já cadastrado';
+			$email_existe = true;
+		}
+		
+	} else {
+		echo 'Erro ao tentar localizar o registro de e-mail';
+	}
+	if($cpf_existe|| $email_existe || $senha_confirmada) {
+		
+		$retorno_get = '';
+		
+		if($cpf_existe){
+			$retorno_get.= "erro_cpf=1&";
+		}
+		
+		if($email_existe){
+			$retorno_get.= "erro_email=1&";	
+		}
+		if ($senha_confirmada){
+			$retorno_get.= "erro_senha=1&";
+		}
+		
+		header('Location: inscrevase.php?'.$retorno_get);
+		die(); //interrompe a execução do script
+	}
+	
+	
+	$sql = " INSERT INTO maf.tb_pessoa
+				(
+				CPF_PESSOA,
+				NOME_PESSOA,
+				EMAIL_PESSOA,
+				DT_NAS_PESSOA,
+				SENHA)
+				VALUES
+				(
+				'$cpfSemMascara',
+				'$nome',
+				'$email',
+				'$dtNascimento',
+				'$senha')";
+
+	//executar a query => a função mysqli_query() espera 2 parâmetros: conexão e a query
+	if (mysqli_query($link,$sql)){
+		echo 'Usuário registrado com sucesso';
+	} else {
+		echo 'Erro ao registrar o usuário';
+	}
+
+?>
