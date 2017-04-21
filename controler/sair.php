67ceaaa9a2b9e540e575675bc7da736baebf603e
Nayara <ncsi.nayara@gmail.com>
2017-04-21 14:00:46 -0300

    Implementado a funcionalidade SAIR.

---
+++ controler/sair.php
@@ -0,0 +1,12 @@
+<?php
+
+	session_start();
+
+	// a função unset elimina indices da super global SESSION
+	unset($_SESSION['NOME_PESSOA']);
+	unset($_SESSION['EMAIL_PESSOA']);
+	unset($_SESSION['ID_PESSOA']);
+	
+	header('Location: ../index.php');
+	
+?>
--- navegacao.php
+++ navegacao.php
@@ -28,7 +28,7 @@ $menuUsuario= isset($_GET['menuUsuario'])			? $_GET['menuUsuario'] 	: 0;
 	            <li <?php if($menuInvest==1) {?>class="active"<?php }?> ><a href="/MinhasAplicacoesFinanceiras/visao/investimentos/investimentos.php?menuInvest=1">Investimentos</a></li>
 	            <li <?php if($menuInst==1) {?>class="active"<?php }?> ><a href="/MinhasAplicacoesFinanceiras/instituicoesFinanceiras.php?menuInst=1">Instituições Financeiras</a></li>
 	            <li <?php if($menuUsuario==1) {?>class="active"<?php }?> ><a href="/MinhasAplicacoesFinanceiras/perfilUsuario/usuario.php?menuUsuario=1"><?= $_SESSION['NOME_PESSOA'] ?></a></li>
-	            <li><a href="sair.php">Sair</a></li>
+	            <li><a href="controler/sair.php">Sair</a></li>
 	          </ul>
 	        </div><!--/.nav-collapse -->
 	      </div>
