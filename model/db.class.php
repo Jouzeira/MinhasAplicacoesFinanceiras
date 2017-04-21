e76908670833351748824999328632e9772942cb
Joseph <joseph.isbir@gmail.com>
2017-04-21 13:45:32 -0300

    Implementação da classe genericoDAO.

--- model/db.class.php
+++ model/db.class.php
@@ -13,6 +13,30 @@ class db {
 
 	//banco de dados
 	private $database = 'maf';
+	
+	private $link;
+	
+	public function __construct(){
+		
+		//criar a conexão
+		// A função mysqli_connect espera 4 parâmetros: localização do banco de dados, usuario de acesso, senha e o banco de dados
+		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+		
+		//ajustar o charset de comunicação entre o charset e o banco de dados , recebe dois parametros: os dados da conexão e qual é o charset
+		mysqli_set_charset($con, 'utf8');
+		
+		//verificar se houve erro de conexão
+		if(mysqli_connect_errno()){
+			echo 'Erro ao tentar se conectar com o banco de dados MySQL '.mysqli_connect_error();
+		}
+		
+		$this->link = $con;
+		
+	}
+	
+	public function getlink(){
+		return $this->link;
+	}
 
 	public function conecta_mysql(){
 
---
+++ model/genericoDao.class.php
@@ -0,0 +1,61 @@
+<?php
+require_once('model/db.class.php');
+// include_once("usuariosVO.php");
+class genericoDAO extends db{
+// 	public function __construct(){}
+// 	private function __clone(){}
+	
+// 	public function __destruct() {
+// 		foreach ($this as $key => $value) {
+// 			unset($this->$key);
+// 		}
+// 		foreach(array_keys(get_defined_vars()) as $var) {
+// 			unset(${"$var"});
+// 		}
+// 		unset($var);
+// 	}
+	
+	public function select($tabela,$fields="*",$where=""){
+// 		$objDB = new db();
+// 		$link = $objDB->conecta_mysql();
+		
+		if(strlen($where)>0) $where= " where ".$where;
+		$sql = "SELECT $fields FROM $tabela$where";
+		
+		return $this->resultado ( $this->getlink(), $sql,"select" );
+		
+	}
+	
+	public function insert($tabela,$fields,$params){
+		
+		$sql = "INSERT INTO $tabela ($fields) VALUES ($params)";
+		$t=$this->insertDB($sql,$params);
+		return $this->resultado ( $this->getlink(), $sql,"insert" );
+	}
+	
+	public function update($tabela,$fields,$where=null){
+		
+		$sql = "UPDATE $tabela SET $fields";
+		if(isset($where)) $sql .= " WHERE $where";
+		return $this->resultado ( $this->getlink(), $sql,"update" );
+	}
+	
+	public function delete($tabela,$where=null){
+		
+		$sql = "DELETE FROM $tabela";
+		if(isset($where)) $sql .= " WHERE $where";
+		return $this->resultado ( $this->getlink(), $sql,"delete" );
+	}
+	
+	private function resultado($link, $sql,$tipoQuery) {
+		if($resultado_id = mysqli_query($link, $sql)){
+			return $resultado_id;
+		}else {
+			echo 'Erro ao executar o select:<br/>'.$sql;
+			die();
+		}
+	}
+}
+?>
+
+
